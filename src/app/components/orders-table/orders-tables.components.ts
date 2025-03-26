aa90dda305f3c1de6a4c210c0c8471335e4efefc
Nicolas Harter <nharter@luccasoftware.com>
2025-03-26 16:46:24 +0100

    feat: add select all

--- src/app/components/orders-table/orders-tables.components.ts
+++ src/app/components/orders-table/orders-tables.components.ts
@@ -1,14 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { Order } from '../types/types';
 import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-orders-table',
   templateUrl: './orders-tables.components.html',
   styleUrls: ['./orders-tables.components.css'],
-  imports: [CheckboxComponent],
+  imports: [CheckboxComponent, FormsModule],
 })
 export class OrdersTableComponent {
   orders = input<Order[]>([]);
+  selectedOrders = signal<Set<string>>(new Set());
+  selectAll = signal<boolean>(false);
 
   formatDate(dateString: string): string {
     const [year, month, day] = dateString.split('-');
@@ -20,5 +24,29 @@ export class OrdersTableComponent {
       style: 'currency',
       currency: 'EUR',
     });
+  }
+
+  toggleSelectAll() {
+    this.selectAll.set(!this.selectAll());
+    if (this.selectAll()) {
+      this.selectedOrders.set(new Set(this.orders().map((order) => order.id)));
+    } else {
+      this.selectedOrders.set(new Set());
+    }
+  }
+
+  toggleOrderSelection(orderId: string) {
+    const currentSelected = this.selectedOrders();
+    if (currentSelected.has(orderId)) {
+      currentSelected.delete(orderId);
+    } else {
+      currentSelected.add(orderId);
+    }
+    this.selectedOrders.set(currentSelected);
+    this.selectAll.set(currentSelected.size === this.orders().length);
+  }
+
+  isOrderSelected(orderId: string): boolean {
+    return this.selectedOrders().has(orderId);
   }
 }
